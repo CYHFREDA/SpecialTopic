de3c835eeb4984051156ae07ef8a045c93cd78bd
Chang Yu Huan <early885@yahoo.com.tw>
2024-09-23 11:21:03 +0800

    Update Book.js

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // 參考 User 模型
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
-module.exports = mongoose.model('Book', bookSchema);
+// 使用 mongoose.models 来避免重定义模型
+const Book = mongoose.models.Book || mongoose.model('Book', bookSchema);
+
+module.exports = Book;
