661c520a0328d3456799cd0e83a80b48c06e1262
Chang Yu Huan <early885@yahoo.com.tw>
2024-09-23 11:31:55 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.json());
 
-// 連接到 MongoDB
-mongoose.connect('mongodb://localhost:27017/bookExchange', { useNewUrlParser: true, useUnifiedTopology: true })
+// 连接到 MongoDB
+mongoose.connect('mongodb://db:27017/book-exchange', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
