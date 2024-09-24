c3c8eab0a7d414c08bc661b4a27e886d91d14fa2
Chang Yu Huan <early885@yahoo.com.tw>
2024-09-24 17:38:36 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+mongoose.set('strictQuery', true);
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const clockSchema = new mongoose.Schema({
