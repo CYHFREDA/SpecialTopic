2b031b778575d747943a975d690f36c7ba136de0
Chang Yu Huan <early885@yahoo.com.tw>
2024-09-24 18:07:24 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ mongoose.set('strictQuery', true);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const cors = require('cors');
+app.use(cors());
+
 const clockSchema = new mongoose.Schema({
     user: String,
     time: Date,
