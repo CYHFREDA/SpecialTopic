b6ccf6ddf9089c05cc786715e6ec84c5dffb2697
Chang Yu Huan <early885@yahoo.com.tw>
2024-10-09 10:08:50 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
-
+const axios = require('axios');
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
