50f86f660e169d4dfc805d34ac9509a1775411a6
Chang Yu Huan <early885@yahoo.com.tw>
2024-10-15 13:39:19 +0800

    Create test.js

---
+++ backend/test.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+// 測試是否能啟動服務
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Server is running!');
+});
+
+server.listen(5001, () => {
+  console.log('測試伺服器在連接埠 5001 上運行');
+});
+
+// 假設您有一個正常執行的代碼功能
+setTimeout(() => {
+  console.log('程式碼運行順利!');
+  process.exit(0); // 正常退出
+}, 1000); // 延遲1秒以確認服務已啟動
