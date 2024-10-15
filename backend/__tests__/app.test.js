25270587846a78ec80d597adffb9c38c75531249
Chang Yu Huan <early885@yahoo.com.tw>
2024-10-15 10:57:18 +0800

    Update app.test.js

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,9 +1,13 @@
-const request = require('supertest'); // 导入 supertest 库
-const app = require('../app'); // 导入您的应用程序代码
+const request = require('supertest');
+const app = require('../app');
+
+jest.mock('mongoose', () => ({
+  connect: jest.fn().mockResolvedValue({}),
+}));
 
 describe('GET /', () => {
   it('should respond with a 200 status code', async () => {
-    const response = await request(app).get('/'); // 发起 GET 请求到根路径
-    expect(response.status).toBe(200); // 期望返回 200 状态码
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
   });
 });
